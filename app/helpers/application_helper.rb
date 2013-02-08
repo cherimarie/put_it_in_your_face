20b07ac61b0f021400773c8928b672dfb81b0887
Strand McCutchen <strand@nird.us>
2013-02-08 13:52:26 -0800

    Created user_sign_in helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def user_sign_in
+    return if params[:controller] == "devise/registrations" ||
+              params[:controller] == "devise/sessions"
+
+    if user_signed_in?
+      "Signed in as #{current_user.email}"
+    else
+      link_to "Sign up", new_user_registration_path
+      link_to "Sign in", new_user_session_path
+    end
+  end
 end
